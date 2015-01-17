562a7add76801c5098013ba91d7990d1742d8f2c
mohammed.ali <mohammed.ali@IHE-MAliDEV.ihorizons.com>
2015-01-18 01:20:41 +0200

    Add Account Create and Edit

---
+++ ViewController/src/com/oracle/anc/fmwpoc/sf/view/beans/MainBean.java
@@ -0,0 +1,31 @@
+package com.oracle.anc.fmwpoc.sf.view.beans;
+
+import javax.faces.event.ActionEvent;
+
+import oracle.ui.pattern.dynamicShell.TabContext;
+
+public class MainBean {
+    public MainBean() {
+    }
+
+    public void launchLeadCreate(ActionEvent actionEvent) {
+        _launchActivity("New Lead", "/WEB-INF/lead-create-btf.xml", true);
+}
+
+    private void _launchActivity(String title, String taskflowId,
+                                 boolean newTab) {
+        try {
+            if (newTab) {
+                TabContext.getCurrentInstance().addTab(title, taskflowId);
+            } else {
+                TabContext.getCurrentInstance().addOrSelectTab(title,
+                                                               taskflowId);
+            }
+        } catch (TabContext.TabOverflowException toe) {
+            // causes a dialog to be displayed to the user saying that there are
+            // too many tabs open - the new tab will not be opened...
+            toe.handleDefault();
+        }
+
+    }
+}
